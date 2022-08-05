cc6098ebda51aadd7c0e5f20974e5866c481520f
Bilal El Yousfi <eybilal@gmail.com>
2022-08-05 20:56:14 +0200

    api-gateway: initial commit

---
+++ api-gateway/src/main/java/com/eybilal/apigateway/rest/CircuitBreakerRestController.java
@@ -0,0 +1,22 @@
+package com.eybilal.apigateway.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class CircuitBreakerRestController {
+
+    @GetMapping(value = "/countries/default")
+    public Map<String, String> defaultCountries() {
+        // In this case we can also use cache to return
+        // the content of the last request
+        Map<String, String> map = new HashMap<>();
+        map.put("message", "Default countries");
+        map.put("countries", "Germany, Luxembourg");
+
+        return map;
+    }
+}
